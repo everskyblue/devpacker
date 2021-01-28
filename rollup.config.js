6ffe591f67dcc2d297eec7bb224aefed7f670c49
Nike Madrid <nikejoelmadrid@gmail.com>
2021-01-28 06:26:12 -0500

    movido al directorio root

---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+export default [{
+    input: './devpacker/loader.js',
+    output: {
+        file: './devpacker/devpacker.bundle.js',
+        format: 'umd',
+        name: 'devpacker',
+    }
+}]
